da5894ad25f5d88433d524ec2bfd6f0d8af3415b
Navleen <navleensandhu0211@gmail.com>
2022-07-18 08:39:44 -0700

    In process to creating a webpage consisting of fun tasks namely:  - age in days  - generator  - rock paper scissors  - blackjack Done with html css part of blackjack task

---
+++ Static/js/script.js
@@ -0,0 +1,151 @@
+//to display age in days
+function getAgeInDays() {
+  let birthYear = prompt("Hey there!....Please enter your year of Birth:)");
+  let ageInDays = (2021 - birthYear) * 365;
+  var h1 = document.createElement("h1"); // <h1></h1>
+  h1.setAttribute("id", "getAge"); //<h1 id="age"></h1>
+  let text = document.createTextNode("You are " + ageInDays + " days old!");
+  h1.appendChild(text);
+  document.querySelector("#flex-box-result").appendChild(h1);
+}
+
+//to reset the text 
+function reset() {
+  document.querySelector("#flex-box-result").remove();
+}
+
+//to generate images
+function generate() {
+  let div = document.querySelector("#generate-image");//<div></div>
+  let img = document.createElement("img");
+  img.src = "https://bestanimations.com/Animals/Mammals/Cats/cats/cute-kitty-animated-gif-26.gif";
+  div.appendChild(img);
+}
+
+//
+function game(yourChoice) {
+  const humanChoice = yourChoice.id;
+  console.log(humanChoice);
+  const computerChoice = numberToChoose(randomToRpsInt());
+  console.log(computerChoice);
+  const results = decideWinner(humanChoice, computerChoice);//[0,1]
+  console.log(results);
+  const message = finalMessage(results);
+  console.log(message);
+  frontEndRps(humanChoice, computerChoice, message);
+}
+
+function randomToRpsInt() {
+  return Math.floor(Math.random() * 3);
+}
+
+function numberToChoose(number) {
+  return ["rock", "paper", "scissors"][number];
+}
+
+function decideWinner(yourChoice, computerChoice) {
+  var rpsData = {
+    "rock": { "rock": 0.5, "paper": 0, "scissors": 1 },
+    "paper": { "rock": 1, "paper": 0.5, "scissors": 0 },
+    "scissors": { "rock": 0, "paper": 1, "scissors": 0.5 },
+  }
+  const yourScore = rpsData[yourChoice][computerChoice];//0
+  const computerScore = rpsData[computerChoice][yourChoice]; //1
+
+  return [yourScore, computerScore];
+}
+
+function finalMessage([yourScore, computerScore]) {
+  if (yourScore === 0) {
+    return { 'message': 'You Lost :(', 'color': 'red' };
+  }
+  else if (yourScore === 0.5) {
+    return { 'message': 'Draw!', 'color': 'yellow' };
+  }
+  else {
+    return { 'message': 'You Won :)', 'color': 'green' };
+  }
+}
+
+function frontEndRps(humanImgChoice, computerImgChoice, finalMessage) {
+  const imagesData = {
+    "rock": document.querySelector("#rock").src,
+    "paper": document.querySelector("#paper").src,
+    "scissors": document.querySelector("#scissors").src
+  }
+
+  //removing images
+  document.querySelector("#rock").remove();
+  document.querySelector("#paper").remove();
+  document.querySelector("#scissors").remove();
+
+  const humanDiv = document.createElement("div");
+  const computerDiv = document.createElement("div");
+  const messageDiv = document.createElement("div");
+
+  humanDiv.innerHTML = "<img src='" + imagesData[humanImgChoice] + "'height= 150px width= 150px;'>"
+  messageDiv.innerHTML = "<h1 style= 'color: " + finalMessage['color'] + "; font-size: 60px; padding: 30px; '>" + finalMessage['message'] + "</h1>"
+  computerDiv.innerHTML = "<img src='" + imagesData[computerImgChoice] + "'height= 150px width= 150px;'>"
+
+  document.querySelector("#flex-box-rps-div").appendChild(humanDiv);
+  document.querySelector("#flex-box-rps-div").appendChild(messageDiv);
+  console.log(messageDiv.innerHTML);
+  document.querySelector("#flex-box-rps-div").appendChild(computerDiv);
+}
+
+//change color of all buttons
+var all_buttons = document.getElementsByTagName("button");
+
+const copyAllButtons = [];
+for (let i = 0; i < all_buttons.length; i++) {
+  copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+function buttonColorChange(buttonThingy) {
+  if (buttonThingy.value === "red") {
+    buttonRed();
+  }
+  else if (buttonThingy.value === "green") {
+    buttonGreen();
+  }
+  else if (buttonThingy.value === "reset") {
+    buttonColorReset();
+  }
+  else if (buttonThingy.value === "random") {
+    randomColors();
+  }
+}
+
+function buttonRed() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add("btn-danger");
+  }
+}
+
+function buttonGreen() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add("btn-success");
+  }
+}
+
+function buttonColorReset() {
+  for (let i = 0; i < all_buttons.length; i++){
+    all_buttons[i].classList.remove(all_buttons[i].classList[1])
+    all_buttons[i].classList.add(copyAllButtons[i])
+  }
+}
+
+function randomColors() {
+  const choices = ["btn-primary", "btn-danger", "btn-success", "btn-warning"];
+  
+  for(let i = 0; i < all_buttons.length; i++){
+  let randomNo = Math.floor(Math.random() * 4);
+  all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+  all_buttons[i].classList.add(choices[randomNo]);
+  }
+}
+
+
+
