7bdb98264ec70b3cba9349a87e4538d82a4719d4
Navleen <navleensandhu0211@gmail.com>
2022-07-22 13:26:20 -0700

    done with JS code functionality and complete UI for the game

--- Projects/Project_RPS/rps.js
+++ Projects/Project_RPS/rps.js
@@ -2,19 +2,74 @@ const rockButtonElement = document.querySelector("#rock");
 const paperButtonElement = document.querySelector("#paper");
 const scissorsButtonElement = document.querySelector("#scissors");
 const resetButtonElement = document.querySelector("#reset");
+const playerSelection = document.querySelector("#player-selection");
+const playerScore = document.querySelector("#updated-score");
+const botSelection = document.querySelector("#bot-selection");
+const displayResult = document.querySelector("#result");
+const displayScore = document.querySelector("#updated-score");
+
+const rock = rockButtonElement.innerText;
+const paper = paperButtonElement.innerText;
+const scissors = scissorsButtonElement.innerText;
 
 rockButtonElement.onclick = () => {
-  console.log("rock");
+  playerSelection.innerText = rock;
+  computerSelection();
+  result();
+  score();
 }
 
 paperButtonElement.onclick = () => {
-  console.log("paper");
+  playerSelection.innerText = paper;
+  computerSelection();
+  result();
+  score();
 }
 
 scissorsButtonElement.onclick = () => {
-  console.log("scissors");
+  playerSelection.innerText = scissors;
+  computerSelection();
+  result();
+  score();
 }
 
 resetButtonElement.onclick = () => {
-  console.log("reset");
+  playerScore.innerText = 0;
+  playerSelection.innerText = "";
+  botSelection.innerText = "";
+}
+
+const computerSelection = () => {
+  const array = [rock, paper, scissors ];
+  let randomNum = Math.floor(Math.random() * 3);
+  let randomEmoji = array[randomNum];
+  botSelection.innerHTML = randomEmoji;
+}
+
+const result = () => {
+  const playerChoice = playerSelection.innerText;
+  const botChoice = botSelection.innerText;
+  if(playerChoice === botChoice){
+    displayResult.innerText = "It's a Draw!";
+
+  }
+  else if(
+    (playerChoice === rock && botChoice === paper) || 
+    (playerChoice === paper && botChoice === scissors) ||
+    (playerChoice === scissors && botChoice === rock)
+  ){
+    displayResult.innerText = "You Lost:(";
+  }
+  else{
+    displayResult.innerText = "You Won:)";
+  }
+}
+
+const score = () => {
+  if(displayResult.innerText === "You Won:)"){
+    displayScore.innerText = Number(displayScore.innerText) + 1;
+  }
+  else if(displayResult.innerText === "You Lost:("){
+    displayScore.innerText = Number(displayScore.innerText) - 1;
+  }
 }
