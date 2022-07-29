aa2ff7962e0022d1ba9d8d2fec12adc47ac4ac5b
Navleen <navleensandhu0211@gmail.com>
2022-07-29 08:10:55 -0700

    added new styling and features to the game UI  - worked on upper display bar for the players

--- projects/fighting_game/game.js
+++ projects/fighting_game/game.js
@@ -27,25 +27,84 @@ const playerB_Health_Div = document.querySelector("#health-player-B");
 const restart_Div = document.querySelector("#restart");
 const playerA_Name_Div = document.querySelector("#player-A");
 const playerB_Name_Div = document.querySelector("#player-B");
+const currentRound_Div = document.querySelector("#current-round");
+const totalRounds_Div = document.querySelector("#total-rounds");
+const pointA = document.querySelector(".pointer-A");
+const pointB  = document.querySelector(".pointer-B");
+const finalResult =  document.querySelector("#result");
 
 const updateUI = () => {
   playerA_Health_Div.innerText = playerA.health;
   playerB_Health_Div.innerText = playerB.health;
   playerA_Name_Div.innerText = playerA.name;
   playerB_Name_Div.innerText = playerB.name;
+  currentRound_Div.innerText = game.round;
+
+  if(game.isPlayerAPlaying){
+    pointA.style.borderRight = "30px solid purple";
+    pointB.style.borderLeft = "30px solid black";
+  }
+  else {
+    pointA.style.borderRight = "30px solid black";
+    pointB.style.borderLeft = "30px solid purple";
+  }
+
+  if(game.round === 0){
+    pointA.style.borderRight = "30px solid black";
+    pointB.style.borderLeft = "30px solid black";
+    finalResult.innerText = game.winner();
+    document.querySelector("#winner").play();
+  }
+
 }
 
 
+
 class Game {
-  constructor(playerA, playerB) {
-    this.isOver = false;
+  constructor(playerA, playerB,maxRound) {
+    this.roundsAreDone = false;
     this.playerA = playerA;
     this.playerB = playerB;
+    this.isPlayerAPlaying = true;
+    this.round = maxRound; 
+    this.maxRound = maxRound;   
   }
 
   restart() {
     this.playerA.reset();
     this.playerB.reset();
+    this.round = this.maxRound;
+    this.isPlayerAPlaying = true;
+    this.roundsAreDone = false;
+  }
+
+  nextTurn() {
+    if(this.isPlayerAPlaying){
+      this.isPlayerAPlaying = false;
+    }
+    else {
+      this.isPlayerAPlaying = true;
+      this.round --;
+      if(this.round === 0){
+        this.roundsAreDone = true;
+      }
+    }
+  }
+
+  isOver() {
+    return this.playerA.health <= 0 || this.playerB.health <= 0 || this.roundsAreDone;
+  }
+
+  winner() {
+    if(this.playerA.health > this.playerB.health){
+      return "Abbie Wins!";
+    }
+    else if(this.playerA.health < this.playerB.health){
+      return "Lisa Wins!";
+    }
+    else {
+      return "It's a Tie";
+    }
   }
 }
 
@@ -59,7 +118,6 @@ class Player {
   hit(enemy) {
     let randomDamage = Math.ceil(Math.random() * 10)
     enemy.takeHit(randomDamage);
-    updateUI();
   }
 
   takeHit(damage) {
@@ -75,7 +133,6 @@ class Player {
         this.health = 100;
       }
     }
-    updateUI();
   }
 
   reset() {
@@ -83,48 +140,51 @@ class Player {
   }
 }
 
-
-
-const playerA = new Player("Abbie", 100);
-const playerB = new Player("Lisa", 100);
-const game = new Game(playerA, playerB);
-
-
+const playerA = new Player("Player A", 100);
+const playerB = new Player("Player B", 100);
+const game = new Game(playerA, playerB, 3);
 
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "a") {
+  if (!game.isOver() && game.isPlayerAPlaying && event.key === "a") {
     playerA.hit(playerB);
+    game.nextTurn();
+    updateUI();
     document.querySelector("#pAhit").play();
   }
 })
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "z") {
+  if (!game.isOver() && game.isPlayerAPlaying && event.key === "z") {
     playerA.lifeline();
+    game.nextTurn();
+    updateUI();
     document.querySelector("#pAlifeline").play();
   }
 })
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "l") {
+  if (!game.isOver() && !game.isPlayerAPlaying && event.key === "l") {
     playerB.hit(playerA);
+    game.nextTurn();
+    updateUI();
     document.querySelector("#pBhit").play();
   }
 })
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "m") {
+  if (!game.isOver() && !game.isPlayerAPlaying && event.key === "m") {
     playerB.lifeline();
+    game.nextTurn();
+    updateUI();
     document.querySelector("#pBlifeline").play();
   }
 })
 
-
 restart_Div.onclick = () => {
   game.restart();
+  finalResult.innerText = "LET'S PLAY AGAIN!";
   updateUI();
 }
-
 
 updateUI()
